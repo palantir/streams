aca742f62e30cd7534ee334a02306dfb73aaa23e
James Baker <j.baker@outlook.com>
2017-07-11 13:34:16 +0100

    Update KeyedStreamTests.java

--- src/test/java/com/palantir/common/streams/KeyedStreamTests.java
+++ src/test/java/com/palantir/common/streams/KeyedStreamTests.java
@@ -167,6 +167,6 @@ public class KeyedStreamTests {
                     result.add(right);
                 });
 
-        assertThat(result).containsExactly(1, 2, 3, 4, 5, 6).inOrder();
+        assertThat(result).containsExactly(1, 4, 2, 5, 3, 6).inOrder();
     }
 }
