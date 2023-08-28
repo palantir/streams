00de99a635d403753a1261da9e9333e96ffa76ec
Andrew Ash <aash@palantir.com>
2023-08-28 15:40:52 -0700

    More cleanup in test

--- streams/src/test/java/com/palantir/common/streams/MoreStreamsTests.java
+++ streams/src/test/java/com/palantir/common/streams/MoreStreamsTests.java
@@ -30,13 +30,7 @@ import java.util.function.UnaryOperator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 public class MoreStreamsTests {
     @Test
     public void testInCompletionOrder_transformWithExecutor() throws InterruptedException {
