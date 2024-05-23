079ecd67bf2177fc9f5983a1f99ac010a6147120
matthew goroff <mgoroff@palantir.com>
2024-05-23 02:39:54 -0400

    save

--- streams/src/test/java/com/palantir/common/streams/MoreStreamsTests.java
+++ streams/src/test/java/com/palantir/common/streams/MoreStreamsTests.java
@@ -20,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
